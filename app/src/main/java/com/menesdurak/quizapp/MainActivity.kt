56c652e8dc8c15a63a9c6187b4c2f9668789ca43
menesdurak <m.enes.durak@gmail.com>
2022-12-14 18:21:30 +0300

    Dummy data added and implemented to main activity

--- app/src/main/java/com/menesdurak/quizapp/MainActivity.kt
+++ app/src/main/java/com/menesdurak/quizapp/MainActivity.kt
@@ -2,10 +2,61 @@ package com.menesdurak.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.menesdurak.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var rightAnswer: String? = null
+    private var rightAnswerCount = 0
+    private var quizCount = 1
+
+    private val quizData = mutableListOf(
+        // country, capital, choice1, choice2, choice3
+        mutableListOf("China", "Beijing", "Jakarta", "Manila", "Stockholm"),
+        mutableListOf("India", "New Delhi", "Beijing", "Bangkok", "Seoul"),
+        mutableListOf("Indonesia", "Jakarta", "Manila", "New Delhi", "Kuala Lumpur"),
+        mutableListOf("Japan", "Tokyo", "Bangkok", "Taipei", "Jakarta"),
+        mutableListOf("Thailand", "Bangkok", "Berlin", "Havana", "Kingston"),
+        mutableListOf("Brazil", "Brasilia", "Havana", "Bangkok", "Copenhagen"),
+        mutableListOf("Canada", "Ottawa", "Bern", "Copenhagen", "Jakarta"),
+        mutableListOf("Cuba", "Havana", "Bern", "London", "Mexico City"),
+        mutableListOf("Mexico", "Mexico City", "Ottawa", "Berlin", "Santiago"),
+        mutableListOf("United States", "Washington D.C.", "San Jose", "Buenos Aires", "Kuala Lumpur"),
+        mutableListOf("France", "Paris", "Ottawa", "Copenhagen", "Tokyo"),
+        mutableListOf("Germany", "Berlin", "Copenhagen", "Bangkok", "Santiago"),
+        mutableListOf("Italy", "Rome", "London", "Paris", "Athens"),
+        mutableListOf("Spain", "Madrid", "Mexico City", "Jakarta", "Havana"),
+        mutableListOf("United Kingdom", "London", "Rome", "Paris", "Singapore")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        showNextQuiz()
+    }
+
+    fun showNextQuiz() {
+        val quiz = quizData[0]
+
+        binding.questionLabel.text = quiz[0]
+
+        binding.answerBtn1.text = quiz[1]
+        binding.answerBtn2.text = quiz[2]
+        binding.answerBtn3.text = quiz[3]
+        binding.answerBtn4.text = quiz[4]
+
+    }
+
+    fun checkAnswer(view: View) {
+
+    }
+
+    fun checkQuizCount () {
+
     }
 }
